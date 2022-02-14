8e72154aed88ff98cd5432c7e1a2f726cf15afe9
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-02-14 15:54:24 +0100

    Create IRepositoryBase.cs

---
+++ StudentTeacher.Repo/StudentTeacher.Repo/GenericRepository/Interface/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTeacher.Repo.GenericRepository.Interface
+{
+    public  interface IRepositoryBase<T>
+    {
+        Task<IQueryable<T>> FindAllAsync(bool trackChanges);
+        Task<IQueryable<T>> FindByConditionAsync(Expression<Func<T, bool>> expression, bool trackChanges);
+        Task CraeteAsync (T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
