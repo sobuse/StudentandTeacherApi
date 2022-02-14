5370ada1f0cc2f73bfe2c0cc9e25231789ab2150
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-02-14 15:53:33 +0100

    Update TeacherDto.cs

--- StudentTeacher.Core/StudentTeacher.Core/DTos/TeacherDto.cs
+++ StudentTeacher.Core/StudentTeacher.Core/DTos/TeacherDto.cs
@@ -26,6 +26,12 @@ namespace StudentTeacher.Core.DTos
     {
         [Required(ErrorMessage = "Teacher name is a required field.")]
         [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
-        public string? Name { get; set; }
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Subject is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for Country is 20 characters.")]
+        public string Subject { get; set; }
+
+        public IEnumerable<StudentCreationDto>? Students { get; set; }
     }
 }
