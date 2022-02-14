cbbe1c315583f091a308b78e0b7d940b29a5b7c5
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-02-14 15:54:19 +0100

    Create StudentData.cs

---
+++ StudentTeacher.Repo/StudentTeacher.Repo/Data/StudentData.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentTeacher.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTeacher.Repo.Data
+{
+    internal class StudentData : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasData(
+                new Student
+                {
+                    Id = 1,
+                    Name = "Azeez",
+                    Class = "Science",
+                    TeacherId = 1
+                },
+                new Student
+                {
+                    Id=2,
+                    Name ="Sammy",
+                    Class ="Management",
+                    TeacherId = 2
+                },
+                new Student
+                {
+                    Id=3,
+                    Name="Obuse",
+                    Class ="Science",
+                    TeacherId=1
+                    
+                }
+                );
+        }
+    }
+}
