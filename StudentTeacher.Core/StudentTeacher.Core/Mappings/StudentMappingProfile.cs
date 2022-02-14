bfc624287c4b440306bd4a8747e025ad4c21e7a9
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-02-14 15:53:42 +0100

    Create StudentMappingProfile.cs

---
+++ StudentTeacher.Core/StudentTeacher.Core/Mappings/StudentMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using StudentTeacher.Core.DTos;
+using StudentTeacher.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTeacher.Core.Mappings
+{
+    public class StudentMappingProfile : Profile
+    {
+        public StudentMappingProfile()
+        {
+            CreateMap<Student, StudentDTo>();
+            CreateMap<StudentCreationDto, Student>();
+            CreateMap<StudentAddUpdateDto, Student>();
+
+            CreateMap<StudentAddUpdateDto, Student>().ReverseMap();
+        }
+    }
+}
