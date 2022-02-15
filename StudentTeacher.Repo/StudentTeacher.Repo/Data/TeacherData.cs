f3956752890831d9a0abd76a3be4121d0e5b53dc
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-02-15 09:17:38 +0100

    seeding data for teachers

---
+++ StudentTeacher.Repo/StudentTeacher.Repo/Data/TeacherData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentTeacher.Core.Models;
+
+namespace StudentTeacher.Repo.Data
+{
+    public class TeacherData : IEntityTypeConfiguration<Teacher>
+    {
+        public void Configure(EntityTypeBuilder<Teacher> builder)
+        {
+            builder.HasData(
+                    new Teacher
+                    {
+                        Id = 1,
+                        Name = "Ehueriken",
+                        Subject = "Maths"
+                    },
+                    new Teacher
+                    {
+                        Id = 2,
+                        Name = "Femi",
+                        Subject = "English"
+                    },
+                    new Teacher { 
+                        Id = 3,
+                        Name ="Chuks",
+                        Subject = "Computer Education"
+                        }
+
+                );
+        }
+    }
+}
