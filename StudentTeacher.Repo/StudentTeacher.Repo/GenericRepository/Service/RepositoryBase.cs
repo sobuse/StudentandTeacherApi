2c731c2edabef1e359ade36ce1b97c1864323b09
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-02-15 15:40:48 +0100

    Create RepositoryBase.cs

---
+++ StudentTeacher.Repo/StudentTeacher.Repo/GenericRepository/Service/RepositoryBase.cs
@@ -0,0 +1,41 @@
+﻿using StudentTeacher.Repo.GenericRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTeacher.Repo.GenericRepository.Service
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext RepositoryContext;
+        public RepositoryBase(RepositoryContext repositoryContext)=>RepositoryContext = repositoryContext;
+
+        public Task CraeteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> FindAllAsync(bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> FindByConditionAsync(Expression<Func<T, bool>> expression, bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
