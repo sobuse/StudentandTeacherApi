e1f847993f76b83a3f2e4e9f867e4175351e6537
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-02-16 20:39:45 +0100

    Create IRepositoryManager.cs

---
+++ StudentTeacher.Service/StudentTeacher.Service/Interfaces/IRepositoryManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTeacher.Service.Interfaces
+{
+    public interface IRepositoryManager
+    {
+        ITeacherRepository Teacher { get; };
+        IStudentRepository Student { get; }
+        Task SaveAsync();
+
+    }
+}
