33304d774c5ce158e9204e5a005fb7c771903574
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-02-16 20:39:52 +0100

    Create IStudentRepository.cs

---
+++ StudentTeacher.Service/StudentTeacher.Service/Interfaces/IStudentRepository.cs
@@ -0,0 +1,16 @@
+﻿using StudentTeacher.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTeacher.Service.Interfaces
+{
+    internal interface IStudentRepository
+    {
+        Task<Student> GetStudent(int teacherId, int studentID, bool trackChanges);
+        Task CraeteStudentForTeacher(int teacherId, Student student);
+        Task DeleteStudent(Student student);
+    }
+}
